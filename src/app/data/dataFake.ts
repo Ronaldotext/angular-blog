8a7d9c476747bd0401ccbf35d3916995324397fc
Ronaldotext <ronaldomarinho27033@gmail.com>
2023-09-13 02:06:14 -0300

    Creating a Blog with Angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake=[
+    {
+        "id":"1",
+        
+        "title":"Capitão América está disponível em Fortnite",
+        "description":" Agora, os jogadores têm a oportunidade de jogar como este icônico super-herói da Marvel, usando seu escudo indestrutível para enfrentar inimigos e proteger o mundo virtual do jogo.",   
+        "photoCover":"https://w0.peakpx.com/wallpaper/446/435/HD-wallpaper-captain-america-2020-captain-america-superheroes-artwork-artist.jpg"
+    },
+    {
+        "id":"2",
+        
+        "title":"Homem de Ferro na lista dos melhores filmes da Marvel",
+        "description":"Homem de Ferro é frequentemente considerado um dos melhores filmes da Marvel. Lançado em 2008, este filme introduziu o personagem Tony Stark, interpretado por Robert Downey Jr., como o Homem de Ferro",
+        "photoCover":"https://image.api.playstation.com/vulcan/img/rnd/202010/2716/5f11i65d9kLRGpvRCbxU6XGE.jpg"
+    },
+    {
+        "id":"3",
+        
+        "title":"Homem-Aranha em Marvel's Avengers",
+        "description":"Em um novo filme emocionante do Universo Cinematográfico Marvel, o Homem-Aranha junta-se aos Vingadores para enfrentar uma ameaça cósmica de proporções épicas.",
+        "photoCover":"https://meups.com.br/wp-content/uploads/2021/11/Homem-Aranha-em-Marvels-Avengers-2.jpg"  
+    },
+    {
+        "id":"4",
+        
+        "title":"Marvel está desenvolvendo filme solo do Hulk",
+        "description":"Neste filme, exploraremos mais a fundo a história do gigante verde, sua luta interna e seu papel como herói no Universo Cinematográfico Marvel.",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/05/Hulk-Vingadores-1024x576.jpg" 
+     }
+]
